40caf57f0668d51431577a984aaa312eb8132939
sclip <oscarkorpi@protonmail.com>
2023-04-28 15:59:45 +0200

    fix: Finished login test

--- src/test/kotlin/AuthenticationTest.kt
+++ src/test/kotlin/AuthenticationTest.kt
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
 import types.auth.DefaultBcryptHasher
+import types.auth.Session
 import types.user.*
 import kotlin.random.Random
 
@@ -52,12 +53,30 @@ class AuthenticationTest {
     }
 
     @Test
-    fun login() {
-
-    }
-
-    @Test
     fun testLogin() {
+        val userCredentials = RawUserCredentials("test@testmail.com", RawPassword("testpassword123"))
+        val sessionDb = mutableMapOf<Session, UserId>()
+        val createSession = { session: Session, userId: UserId ->
+            sessionDb[session] = userId
+        }
+
+        val existingTestUser = UserImpl(null, object : UserDetails {}, userCredentials.hash(DefaultBcryptHasher))
+        val id = addToDb(existingTestUser)
+
+        val session = auth.login(userCredentials, createSession)
+        assertTrue(session.isRight(), "Login should have been successful")
+        assertEquals(id, sessionDb[session.getOrNull()!!])
+        sessionDb.clear()
+
+        val invalidLoginCredentials = RawUserCredentials("wrong@testmail.com", RawPassword("testpassword123"))
+        val invalidLogin = auth.login(invalidLoginCredentials, createSession)
+        assertTrue(invalidLogin.isLeft())
+        assertInstanceOf(AuthenticationException.UserNotFound::class.java, invalidLogin.leftOrNull())
+        sessionDb.clear()
+        val wrongPasswordCredentials = RawUserCredentials("test@testmail.com", RawPassword("wrongpassword123"))
+        val wrongPasswordLogin = auth.login(wrongPasswordCredentials, createSession)
+        assertTrue(wrongPasswordLogin.isLeft())
+        assertInstanceOf(AuthenticationException.InvalidCredentials::class.java, wrongPasswordLogin.leftOrNull())
     }
 
     @Test
